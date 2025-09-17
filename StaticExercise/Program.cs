6dfe3d6036cdce16896477c72da66b00e2f52efe
Garrett Chitwood <gman@acvwa.com>
2025-09-17 12:55:13 -0700

    Created a fahrenheit and celsius conversion program, demonstrated several value conversions as well.

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(75);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(30);
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine($"Celsius: {celsius}");
+            Console.WriteLine($"Fahrenheit: {fahrenheit}");
+            
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("");
+            
+            var celsiusEquiv = TempConverter.FahrenheitToCelsius(50);
+            var fahrenheitEquiv = TempConverter.CelsiusToFahrenheit(10);
+            Console.WriteLine($"The following converted values of Celsius and Fahrenheit are equivalent:");
+            Console.WriteLine($"Celsius base value: {fahrenheitEquiv}, and Fahrenheit base value: {celsiusEquiv}");
+            
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("");
         }
     }
 }
